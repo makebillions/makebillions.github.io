5d5df1c85cd37313871122a0ff6aa9a592d91254
makebillions <162313712+makebillions@users.noreply.github.com>
2025-09-11 00:38:01 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -231,7 +231,7 @@ function toggleSwitch(toggleId) {
 // const host = process.env.REACT_APP_API;
 // const host = "http://localhost:4000";
 // const host = "https://modern-vocal-reptile.ngrok-free.app";
-const host = "https://aba84f642cec.ngrok-free.app";
+const host = "https://fe05f8f3488e.ngrok-free.app";
 function url(url) {
     return `${host}${url}`;
 }
