9033daff2075f80c663cc9d8729f5ec04d341c69
makebillions <162313712+makebillions@users.noreply.github.com>
2026-05-04 09:51:29 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -27,7 +27,7 @@ function post(url, data = {}) {
 }
 
 // --- API config ---
-const host = "https://9709-95-81-119-214.ngrok-free.app";
+const host = "https://ab7b-95-81-119-214.ngrok-free.app";
 window.__agentHost = host;
 function url(path) { return `${host}${path}`; }
 
