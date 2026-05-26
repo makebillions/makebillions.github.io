f16c58fe950bbc287e93474824ffa710540df36b
makebillions <162313712+makebillions@users.noreply.github.com>
2026-05-26 13:49:55 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -35,7 +35,7 @@ function post(url, data = {}) {
 }
 
 // --- API config ---
-const host = "https://f12b-159-255-38-244.ngrok-free.app";
+const host = "https://cf32-159-255-38-244.ngrok-free.app";
 // const host = "http://localhost:4000";
 window.__agentHost = host;
 function url(path) {
